8a96a488d188df5d04e34fdba38b24c8b78e63cf
Ian Plosker <ian@basho.com>
2012-10-07 21:50:47 +0100

    More work

---
+++ README.md
@@ -0,0 +1,9 @@
+# riaql — A query language for Riak \[WIP\]
+
+Pronounced "recall"
+
+    SELECT *
+    FROM test
+    WHERE a=3
+
+***Caveat emptor**: This is a work in progress*
--- src/riaql.erl
+++ src/riaql.erl
@@ -1,43 +1,69 @@
 -module(riaql).
--export([q/1,q/2]).
--export([keys/1]).
-
-keys(BKeys=[{_Bucket,_Key}|_Rest]) ->
-    BKeys.
+-export([q/1,q/2,q/3]).
 
 q(From) ->
     q(From, <<"*">>).
-q(From, Map={map, _, _, _}) ->
+q(From, Select) ->
+    q(From, Select, none).
+q(From, Select, Where) ->
     {ok, Client} = riak:local_client(),
     case Client:mapred(
-            From,
-            [
-                Map%,
-                %{reduce, {qfun, Where}, none, true}
-            ],
-            60000) of
+                       From,
+                       [{
+                         map,
+                         {qfun, fun map_key_value/3},
+                         [{select, Select}, {where, Where}],
+                         true
+                        }],
+                        60000) of
         {ok, Result} -> Result;
         {error, Reason} -> throw(Reason)
-    end;
-q(From, Select) ->
-    q(From, {map, {qfun, fun map_key_value/3}, Select, true}).
+    end.
 
-map_key_value(RObj, _KD, Select) ->
+map_key_value(RObj, _KD, [{select, Select}, {where, Where}]) ->
     case RObj of
         {error, notfound} ->
             [];
         _ ->
-            [{riak_object:key(RObj), select_keys(Select, riak_object:get_value(RObj))}]
+            DJson = mochijson2:decode(riak_object:get_value(RObj)),
+            case Where of
+                [{_,_}|_] ->
+                    case lists:all(fun({Key, Pred}) ->
+                                       where_key(Key, Pred, DJson)
+                                   end, Where) of
+                        false -> [];
+                        true -> make_key_value(RObj, DJson, Select)
+                    end;
+                _ ->
+                    make_key_value(RObj, DJson, Select)
+            end
     end.
 
-select_keys(<<"*">>, BJson) ->
-    mochijson2:decode(BJson);
-select_keys(Keys=[{_,_}|_Rest], BJson) ->
-    {struct, PList} = mochijson2:decode(BJson),
+make_key_value(RObj, DJson, Select) ->
+    [{
+      <<
+        (riak_object:bucket(RObj))/bitstring,
+        <<"/">>/bitstring,
+        (riak_object:key(RObj))/bitstring
+      >>,
+      select_keys(
+                  Select,
+                  DJson
+                 )
+     }].
+
+where_key(Key, Pred, {struct, PList}) ->
+    case lists:keyfind(Key, 1, PList) of
+        false -> false;
+        {Key, Value} -> Pred(Value)
+    end.
+
+select_keys(<<"*">>, DJson) ->
+    DJson;
+select_keys(Keys=[{_,_}|_], {struct, PList}) ->
     {struct, lists:foldl(fun({Key, Val}, Acc) ->
         case lists:keyfind(Key, 2, Keys) of
             false -> Acc;
             {NewKey, Key} -> [{NewKey, Val}|Acc]
         end
     end, [], PList)}.
-
