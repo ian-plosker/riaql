7d86bc1acf46082ac9bace56ffd831bd5351509c
Ian Plosker <ian@basho.com>
2012-10-19 18:58:10 +0100

    Separate clauses into phases

--- src/riaql_pipe.erl
+++ src/riaql_pipe.erl
@@ -5,30 +5,61 @@
 -include_lib("riak_pipe/include/riak_pipe_log.hrl").
 
 process(Query) ->
+    Select = proplists:get_value(select, Query),
+
     PipeSpec = [#fitting_spec{name=fetch,
                    module=riak_kv_pipe_get,
                    chashfun=follow,
                    nval={riak_kv_pipe_get, bkey_nval}},
-                #fitting_spec{name=select,
-                   module = riak_pipe_w_xform,
-                   arg=fun(Input, Partition, FittingDetails) ->
-                           Results = map(Input, FittingDetails, Query),
-                           send_results(Results, Partition, FittingDetails)
-                       end,
+                #fitting_spec{name=decode,
+                   module=riak_pipe_w_xform,
                    chashfun=follow,
+                   arg=fun(Input, Partition, FittingDetails) ->
+                           Results = make_key_value(erlang_input(Input)),
+                           send_results(Results, Partition, FittingDetails)
+                   end,
                    q_limit = 64,
-                   nval=1}],
+                   nval = 1}
+                    ] ++
+            case proplists:get_value(where, Query) of
+                Where=[{_,_}|_] ->
+                    [#fitting_spec{name=where,
+                        module = riak_pipe_w_xform,
+                        arg=fun(Input, Partition, FittingDetails) ->
+                           Results = filter(Input, FittingDetails, Where),
+                           send_results(Results, Partition, FittingDetails)
+                        end,
+                        chashfun=follow,
+                        q_limit = 64,
+                        nval=1}];
+                _ ->
+                    []
+            end
+            ++ case proplists:get_value(select, Query) of
+                <<"*">> ->
+                    [];
+                Select ->
+                    [#fitting_spec{name=select,
+                        module = riak_pipe_w_xform,
+                        arg=fun(Input, Partition, FittingDetails) ->
+                           Results = map(Input, Select),
+                           send_results(Results, Partition, FittingDetails)
+                        end,
+                        chashfun=follow,
+                        q_limit = 64,
+                        nval=1}]
+            end,
     {ok, Pipe} = riak_pipe:exec(PipeSpec, []),%[{trace, all},{log, lager}])
-    case From = proplists:get_value(from, Query) of
+    case proplists:get_value(from, Query) of
         {index, Bucket, Index, Match} ->
             riak_kv_pipe_index:queue_existing_pipe(Pipe, Bucket, {eq, Index, Match}, 60000);
         {index, Bucket, Index, Start, End} ->
             riak_kv_pipe_index:queue_existing_pipe(Pipe, Bucket, {range, Index, Start, End}, 60000);
-        Bucket when is_binary(Bucket) ->
+        From when is_binary(From) ->
             riak_kv_pipe_listkeys:queue_existing_pipe(Pipe, From, 60000)
     end,
     {_, Results, _} = riak_pipe:collect_results(Pipe),
-    Results.
+    [Result || {_, Result} <- Results].
 
 send_results([], _, _) ->
     ok;
@@ -40,44 +71,35 @@ send_results([Result|Results], P, FD)  ->
             throw(ER)
     end.
 
-map(Input,_,Args) ->
-    map_key_value(erlang_input(Input), none, Args).
+map({Key,Value}, Select) ->
+    [{Key, select_keys(Select, Value)}].
 
-map_key_value({error, notfound}, _, _) ->
+filter({_Key,Value}=Input, _, Where) ->
+    case lists:all(fun({Key, Pred}) ->
+                    where_key(Key, Pred, Value)
+            end, Where) of
+        false -> [];
+        true -> [Input]
+    end.
+
+make_key_value({error, notfound}) ->
     [];
-map_key_value(RObj, _KD, Query) ->
+make_key_value(RObj) ->
     case (catch mochijson2:decode(riak_object:get_value(RObj))) of
         {'EXIT', _} ->
-            lager:info("Dail");
+            [];
         DJson={struct, _} ->
-            Select = proplists:get_value(select, Query),
-            case proplists:get_value(where, Query) of
-                Where=[{_,_}|_] ->
-                    case lists:all(fun({Key, Pred}) ->
-                                       where_key(Key, Pred, DJson)
-                                   end, Where) of
-                        false -> [];
-                        true -> make_key_value(RObj, DJson, Select)
-                    end;
-                _ ->
-                    make_key_value(RObj, DJson, Select)
-            end;
+            [{
+              <<
+                (riak_object:bucket(RObj))/bitstring,
+                <<"/">>/bitstring,
+                (riak_object:key(RObj))/bitstring
+              >>,
+              DJson
+            }];
         _ ->
             []
     end.
-
-make_key_value(RObj, DJson, Select) ->
-    [{
-      <<
-        (riak_object:bucket(RObj))/bitstring,
-        <<"/">>/bitstring,
-        (riak_object:key(RObj))/bitstring
-      >>,
-      select_keys(
-                  Select,
-                  DJson
-                 )
-     }].
 
 where_key(Key, Pred, {struct, PList}) ->
     case lists:keyfind(Key, 1, PList) of
@@ -85,8 +107,6 @@ where_key(Key, Pred, {struct, PList}) ->
         {Key, Value} -> Pred(Value)
     end.
 
-select_keys(<<"*">>, DJson) ->
-    DJson;
 select_keys(Keys=[{_,_}|_], {struct, PList}) ->
     {struct, lists:foldl(fun({Key, Val}, Acc) ->
         case lists:keyfind(Key, 2, Keys) of
@@ -95,13 +115,7 @@ select_keys(Keys=[{_,_}|_], {struct, PList}) ->
         end
     end, [], PList)}.
 
-bucket(Bucket) ->
-    Bucket.
-
-index(Bucket, Index, Key) ->
-    {index, Bucket, Index, Key}.
-index(Bucket, Index, StartKey, EndKey) ->
-    {index, Bucket, Index, StartKey, EndKey}.
-
-erlang_input({ok, Input, _})          -> Input;
-erlang_input({{error,_}=Input, _, _}) -> Input.
+erlang_input({ok, Input, _}) ->
+    Input;
+erlang_input({{error,_}=Input, _, _}) ->
+    Input.
